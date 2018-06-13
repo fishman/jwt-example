a88813f7e7d43e61250763723e3d3d4cfe628be1
Reza Jelveh <toyb0mb@gmx.net>
2018-06-13 14:44:57 +0200

    bugfixes

--- prep.sh
+++ prep.sh
@@ -1,3 +1,6 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
+go get -u github.com/dgrijalva/jwt-go
+go get -u golang.org/x/net/context
+go get -u google.golang.org/grpc
