f3fb4cfe1a607713532a15c8ffd1b51553e54f6b
Reza Jelveh <toyb0mb@gmx.net>
2018-06-13 21:54:22 +0200

    backend: add rest api

---
+++ backend/app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,8 @@
+module V1
+  class MealsController < ApplicationController
+
+    # GET /meals
+    def index
+    end
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -7,6 +7,14 @@ Rails.application.routes.draw do
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   end
+  scope '/api' do
+    # Version 1 of API
+    scope module: :v1, constraints: ApiConstraint.new(version: 1) do
+      scope '/v1' do
+        # mount_devise_token_auth_for 'User', at: 'auth'
+        resources :greetings, only: [:index]
+      end
+    end
 
   # get '*all', to: 'application#index'
 end
